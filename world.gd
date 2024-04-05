a08c6f16cf28e7abc1f56e410a8babad219beed3
ProjectPybro <67141208+ProjectPybro@users.noreply.github.com>
2024-04-05 02:14:47 +0100

    Added some TODO's

--- world.gd
+++ world.gd
@@ -9,12 +9,14 @@ var ball_scene: PackedScene = load("res://scenes/ball.tscn")
 func set_score(value):
 	score = value
 	## get_node("Score").set_text("Score: "+str(score))
+	## TODO: Add Score + Ball Count?
 
 
 func _on_ball_timer_timeout():
 	# #2) Creates a new instance of the ball
 	var ball = ball_scene.instantiate() 
 	ball.set_position(Vector2(100, 100))
+	## TODO: Randomise the position of the wall
 	## 3) Add ball to the scene tree
 	$BallList.add_child(ball) # Adds the ball to the scene tree
 	## print("Debug: Ball Spawned")
