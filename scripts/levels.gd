742ea7e2e12dee08823f4254c5434711d615e83f
ProjectPybro <67141208+ProjectPybro@users.noreply.github.com>
2024-04-12 17:00:58 +0100

    Removed comment
    It was no longer true since the variable was renamed

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -1,6 +1,6 @@
 class_name Level
 
 var number_of_rows := 0  # Rows - left to right
-var bricks_per_row := 0 # columns - top to bottom
+var bricks_per_row := 0 
 var spacing_between_rows := 0
 var spacing_between_columns := 0
