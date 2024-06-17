cf008d9d22ca95fff004ebeba0264f0306be5262
ProjectPybro <67141208+ProjectPybro@users.noreply.github.com>
2024-06-17 17:50:11 +0100

    Added 5 stages of balloon HP
    BTD style HP. Red, Blue, Green, Yellow, and Pink

--- scenes/brick/brick.gd
+++ scenes/brick/brick.gd
@@ -1,19 +1,19 @@
 extends StaticBody2D
 class_name Brick
 
-@export var health = 1
+@export var health = 5
 var worth_in_points = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	$BrickSprite.set_frame(4)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
+
 func remove_health():
 	if is_queued_for_deletion() == true:
 		return ## I think this fixes a bug where the brick counter would go negative
@@ -22,11 +22,13 @@ func remove_health():
 	if health <= 0:
 		queue_free()
 		Global.brick_destroyed.emit(worth_in_points)
+		return
+	
+	$BrickSprite.set_frame($BrickSprite.get_frame() - 1)
+	
 
 
 ## TODO: Add a respawning mechanic
 ## TODO: Add different colored bricks with different hp's
 ## TODO: Brick Spawner should spawn different colored bricks
 ## TODO: Do we want composition or is that too much?
-	
-	
